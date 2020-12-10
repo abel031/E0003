858cdbcbc65e1d2f4f4ece8e40179a9f04dcc236
abel031 <abel031@users.noreply.github.com>
2020-12-10 10:43:28 +0100

    10/12/20 antes pati

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'E0003';
+  title = 'Abel';
+  place = {
+    city: "Valencia",
+    cp: "46000"
+  }
+  entrada = "cod"
+  valorhola = "123456"
+  disabled = false
+  color="color2"
+
+  miColor = true;
+
+  estilos = {
+    "azul": this.miColor,
+    "rojo": !this.miColor
+  }
+
+  onClick(event){
+    console.log("hola"+event+" "+event.type)
+  }
+
+  cambio(){
+    console.log("HA CAMBIADO")
+  }
 }

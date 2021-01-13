31e5e313d76ccd4496880997d335b2ddfc79a9df
abel031 <abel031@users.noreply.github.com>
2021-01-13 12:24:43 +0100

    13/01/2021 antes clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from './depto/usuarios.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { UsuariosService } from './depto/usuarios.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   constructor(private _usuarios:UsuariosService){}
 
